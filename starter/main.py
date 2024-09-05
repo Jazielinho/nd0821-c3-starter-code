872ef6beb797d34c5e9f463697331245a4d46f8d
jahaziel <jahaziel@senstile.com>
2024-09-05 17:11:01 +0200

    triyng for render2

--- starter/main.py
+++ starter/main.py
@@ -18,18 +18,28 @@ logger = logging.getLogger(__name__)
 def run_dvc_commands():
     try:
         # Crear directorios temporales
+        logger.info("Creando directorios temporales...")
         os.makedirs('/tmp/dvc-cache', exist_ok=True)
+        logger.info("Directorios temporales creados1.")
         os.makedirs('/tmp/dvc-tmp', exist_ok=True)
+        logger.info("Directorios temporales creados2.")
         os.makedirs('/tmp/dvc-state', exist_ok=True)
+        logger.info("Directorios temporales creados3.")
 
         # Configurar DVC para usar los directorios temporales
+        logger.info("Configurando DVC...")
         subprocess.run(['dvc', 'config', 'cache.dir', '/tmp/dvc-cache'], check=True)
+        logger.info("DVC configurado.")
 
+        logger.info("Ejecutando dvc pull...")
         env = os.environ.copy()
         env['DVC_TMP_DIR'] = '/tmp/dvc-tmp'
         env['DVC_STATE_DIR'] = '/tmp/dvc-state'
+        logger.info("Ejecutando dvc pull...")
 
+        logger.info("Ejecutando dvc pull...")
         subprocess.run(['dvc', 'pull'], env=env, check=True)
+        logger.info("dvc pull completado.")
     except subprocess.CalledProcessError as e:
         logger.error(f"Error al ejecutar dvc pull: {e}")
         raise
