ec7c748167b0fac98c21bbd010894b17412896bf
jahaziel <jahaziel@senstile.com>
2024-09-05 18:13:30 +0200

    triyng for render 7

--- run_dvc_and_start.sh
+++ run_dvc_and_start.sh
@@ -9,9 +9,11 @@ echo "Configurando DVC para usar los directorios temporales..."
 dvc cache dir /tmp/dvc-cache
 export DVC_TMP_DIR=/tmp/dvc-tmp
 export DVC_STATE_DIR=/tmp/dvc-state
+export DVC_HOME=/tmp/dvc-home  # Forzamos que DVC use /tmp para todas las rutas de estado
+mkdir -p /tmp/dvc-home
 
 echo "Ejecutando dvc pull..."
-dvc pull
+DVC_TMP_DIR=/tmp/dvc-tmp DVC_STATE_DIR=/tmp/dvc-state DVC_HOME=/tmp/dvc-home dvc pull --no-scm
 
 # Comprobar si dvc pull fue exitoso
 if [ $? -eq 0 ]; then
